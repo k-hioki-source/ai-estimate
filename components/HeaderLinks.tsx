747b2afa77f97f634d10653b5f04a8da4e7d07c5
k-hioki-source <k-hioki@create-support.co.jp>
2026-06-17 09:15:22 +0900

    Update HeaderLinks.tsx

--- components/HeaderLinks.tsx
+++ components/HeaderLinks.tsx
@@ -1,1 +1,21 @@
+export default function HeaderLinks() {
+  return (
+    <div className="cs-header-links">
+      <a
+        href="https://www.create-support.co.jp/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        会社サイトへ戻る →
+      </a>
 
+      <a href="/privacy-policy">
+        プライバシーポリシー
+      </a>
+
+      <a href="/terms">
+        利用規約
+      </a>
+    </div>
+  );
+}
